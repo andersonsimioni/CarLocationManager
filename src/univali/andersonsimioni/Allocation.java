3c6f794aa0cdfa29359c8af8f97eede924ab810b
Bit <willian.souza.ti@gmail.com>
2020-09-07 20:47:53 -0300

    add penalty calcs and display to allocation

--- src/univali/andersonsimioni/Allocation.java
+++ src/univali/andersonsimioni/Allocation.java
@@ -1,6 +1,7 @@
 package univali.andersonsimioni;
 
 import java.time.LocalDate;
+import java.time.Period;
 
 public class Allocation {
     private final LocalDate AllocationTime;
@@ -27,7 +28,7 @@ public class Allocation {
         if(car == null)
             throw new IllegalArgumentException("Car is null");
 
-        if(car.getStatus().equals("Allocated"))
+        if(car.getStatus().equals(univali.andersonsimioni.Car.AvailableStatus.Allocated.name()))
             throw new IllegalArgumentException("Car already allocated");
 
         if(client == null)
@@ -38,6 +39,41 @@ public class Allocation {
 
         if(securityDeposit <= car.calculateDailyPrice(1.12f)*pretenseOfTime)
             throw new IllegalArgumentException("Security Deposit is lower than minimum, minimum required value: "+(car.calculateDailyPrice(1.12f)*pretenseOfTime));
+    }
+
+    public String getDisplayInfo(){
+        return "Allocation{\n" +
+                "   allocation time: " + this.getAllocationTime() + "\n" +
+                "   Pretense of time: " + this.getPretenseOfTime() + "\n" +
+                "   Goal: " + this.getGoal() + "\n" +
+                "   Security deposit: " + this.getSecurityDeposit() + "\n" +
+                "   unallocation time: " + this.getUnallocateTime() + "\n" +
+                this.getCar().getDisplayInfo() + ",\n" +
+                this.getClient().getDisplayInfo() + ",\n" +
+                "}";
+    }
+
+    public void unallocate(){
+        //just for tests
+        UnallocateTime = LocalDate.now().plusDays(3);
+        Car.setStatus(univali.andersonsimioni.Car.AvailableStatus.Unallocated);
+    }
+
+    public String getNote(){
+        if(this.getUnallocateTime() == null){
+            throw new IllegalStateException("Note available only after vehicle return");
+        }
+
+        String status = (this.getUnallocateTime() == null) ? "Current" : "Ended at: "+this.getUnallocateTime();
+        Period period = Period.between(this.getAllocationTime(), this.getUnallocateTime());
+        float finalValue = period.getDays() * this.getCar().calculateDailyPrice(1.12f);
+
+        return "Allocation{\n" +
+                "   status: " + status + "\n" +
+                "   use period: " + period.getDays() + " days\n" +
+                "   final value: " + finalValue + "\n" +
+                "   difference between security deposit: " + (this.getSecurityDeposit() - finalValue) + "\n" +
+                "}";
     }
 
     public LocalDate getUnallocateTime() {
@@ -70,5 +106,17 @@ public class Allocation {
 
     public void setSecurityDeposit(float securityDeposit) {
         SecurityDeposit = securityDeposit;
+    }
+
+    public LocalDate getAllocationTime() {
+        return AllocationTime;
+    }
+
+    public univali.andersonsimioni.Client getClient() {
+        return Client;
+    }
+
+    public univali.andersonsimioni.Car getCar() {
+        return Car;
     }
 }
--- src/univali/andersonsimioni/SystemTest/UseSimulation.java
+++ src/univali/andersonsimioni/SystemTest/UseSimulation.java
@@ -20,32 +20,28 @@ public class UseSimulation {
         Car allocationCar1 = new Car(model1, "MFW-7033", "Black", "A254SDCASD", "164549616161", 90000);
         Car allocationCar2 = new Car(model2, "ABC-2021", "White", "UGSAD12312", "456788972131", 23000);
 
+        /*
         System.out.println("Show registered clients:");
         System.out.println(client1.getDisplayInfo());
         System.out.println(client2.getDisplayInfo());
 
         System.out.println("Show registered cars:");
         System.out.println(allocationCar1.getDisplayInfo());
-        System.out.println(allocationCar2.getDisplayInfo());
+        System.out.println(allocationCar2.getDisplayInfo());*/
 
         System.out.println("Start allocation!");
         Allocation allocation = new Allocation(
                 client2,
                 allocationCar2,
-                2,
+                4,
                 "Travel with family to south",
-                100
+                200
         );
+        System.out.println(allocation.getDisplayInfo());
+        allocation.unallocate();
+        System.out.println(allocation.getNote());
         System.out.println("Success!");
-        System.out.println("Start allocation!");
-        Allocation allocation2 = new Allocation(
-                client1,
-                allocationCar2,
-                2,
-                "Travel with family to south",
-                100
-        );
-        System.out.println("Success!");
+
 
     }
 }
